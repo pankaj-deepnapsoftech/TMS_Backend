a5273ee1ed0eb09483062ac03e26d281e5543b30
vishalpanchal1024 <vishalpanchal1024@gmail.com>
2025-07-03 16:10:20 +0530

    hjdgh

--- src/controller/DashboardController.js
+++ src/controller/DashboardController.js
@@ -4,7 +4,7 @@ export const getDashboardStats = async (req, res) => {
   try {
     const total = await Ticket.countDocuments();
     const open = await Ticket.countDocuments({ status: 'Open' });
-    const resolved = await Ticeket.countDocumnts({ status: 'Resolved' });
+    const resolved = await Ticket.countDocumnts({ status: 'Resolved' });
     const inProgress = await Ticket.countDocuments({ status: 'In Progress' });
 
     res.json({
@@ -16,4 +16,5 @@ export const getDashboardStats = async (req, res) => {
   } catch (err) {
     res.status(500).json({ message: 'Server error', error: err.message });
   }
-};
+}; 
+
