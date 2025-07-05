4893d1bb9dc6d59fe2ad08bc2bb929302946215e
vishalpanchal1024 <vishalpanchal1024@gmail.com>
2025-07-05 10:50:21 +0530

    gghvjhj

--- src/controller/DashboardController.js
+++ src/controller/DashboardController.js
@@ -15,10 +15,9 @@ export const getDashboardStats = async (req, res) => {
       total,
       open,
       resolved,
-      inProgress,  
+      inProgress,
     });
   } catch (err) {
     res.status(500).json({ message: 'Server error', error: err.message });
   }
 };
-      
