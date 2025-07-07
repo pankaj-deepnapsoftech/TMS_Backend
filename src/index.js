b8c9d61871620a57592200c2d9772464089b07e2
nitinthakurdev <radheradhe9870023@gmail.com>
2025-07-07 14:15:55 +0530

    new changes

--- src/index.js
+++ src/index.js
@@ -6,11 +6,22 @@ import authRoutes from './routes/authRoutes.js';
 import ticketRoutes from './routes/ticketRoutes.js';
 import dashboardRoutes from './routes/dashboardRoutes.js';
 import notificationRoutes from './routes/notificationRoutes.js';
+import { config } from 'dotenv';
 
-const PORT = 5000;
+const PORT = 8093;
 connectDB();
 const app = express();
-app.use(cors());
+
+app.use(
+  cors({
+    origin:
+      config.NODE_ENV !== 'development'
+        ? config.CLIENT_URL
+        : config.CLIENT_URL_LOCAL,
+    methods: ['POST', 'PUT', 'PATCH', 'DELETE', 'GET', 'OPTIONS'],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
