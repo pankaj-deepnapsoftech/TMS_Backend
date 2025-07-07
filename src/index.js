6522415fb777166f5caf3f15af4e94e82c8426ae
nitinthakurdev <radheradhe9870023@gmail.com>
2025-07-07 14:16:06 +0530

    newchnags

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ import authRoutes from './routes/authRoutes.js';
 import ticketRoutes from './routes/ticketRoutes.js';
 import dashboardRoutes from './routes/dashboardRoutes.js';
 import notificationRoutes from './routes/notificationRoutes.js';
-import { config } from 'dotenv';
+import { config } from './config/env.config.js';
 
 const PORT = 8093;
 connectDB();
 const app = express();
-
+app.use(express.json());
 app.use(
   cors({
     origin:
@@ -22,8 +22,6 @@ app.use(
     credentials: true,
   })
 );
-app.use(express.json());
-
 app.use('/api/auth', authRoutes);
 app.use('/api/tickets', ticketRoutes);
 app.use('/api/dashboard', dashboardRoutes);
